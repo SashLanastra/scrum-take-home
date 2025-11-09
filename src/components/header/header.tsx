89683e31bcdd4cc937cbe55d2e1ee6dd31667630
SashLanstra <mphomankanda95@gmail.com>
2025-11-09 11:56:17 +0200

    Remove commented-out code for the CustomButton and AddIcon in the Header component to clean up the codebase.

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,17 +1,9 @@
 import "./header.scss";
-import { Add as AddIcon } from "@mui/icons-material";
-import { CustomButton } from "../custom-button/custom-button";
 
 export const Header = () => {
   return (
     <header className="navbar">
       <img src="/assets_logo.svg" alt="logo" width={200} height={100} className="navbar-logo" />
-      {/* <div className="navbar-actions">
-        <CustomButton customVariant="fill" customSize="medium">
-          <AddIcon fontSize="inherit" sx={{ color: "#F79E00" }} />
-          add asset
-        </CustomButton>
-      </div> */}
     </header>
   );
 };
